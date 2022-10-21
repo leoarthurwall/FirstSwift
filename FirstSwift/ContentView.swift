ffe9cf431a8082a13a2741dc45a4056b8e72d82b
leoarthurwall <leowall93@gmail.com>
2022-10-21 16:26:10 +0100

    learned how to import images into the asset folder

--- FirstSwift/ContentView.swift
+++ FirstSwift/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello").padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/).background(Color.green).blur(radius: 1.0)
+        Text("Hello")
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .background(Color.green)
+            .blur(radius: 1.0)
     }
 }
 
