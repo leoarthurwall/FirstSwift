05e86d46241d431bb012346cebed0ded9ecc05b3
leoarthurwall <leowall93@gmail.com>
2022-10-21 16:03:57 +0100

    playing around with the swift UI kit, learning how to add and amend modifiers

--- FirstSwift/ContentView.swift
+++ FirstSwift/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Family!")
-            .padding()
+        Text("Hello").padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/).background(Color.green).blur(radius: 1.0)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro Max")
     }
 }
