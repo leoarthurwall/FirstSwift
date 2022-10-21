88f11e35137687c0ae2d1ee47a23dc7bde0f619c
leoarthurwall <leowall93@gmail.com>
2022-10-21 15:09:54 +0100

    Initial Commit

---
+++ FirstSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstSwift
+//
+//  Created by Leo Wall on 21/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstSwift/FirstSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftApp.swift
+//  FirstSwift
+//
+//  Created by Leo Wall on 21/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
